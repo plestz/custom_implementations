5c405c0029e35943dc3a03b60788faf11f7c25c4
plestz <96513494+plestz@users.noreply.github.com>
2025-01-07 21:57:45 -0500

    feat: main kolmogorov-arnold network class complete, integrated with all KANLayers; feat: introduced weighted basis function to activation functions as building block

--- kolmogorov_arnold_network/kan.py
+++ kolmogorov_arnold_network/kan.py
@@ -9,7 +9,7 @@ class UnivariateFunction(nn.Module):
     With respect to the official KAN paper (linked in repository README),
     UnivariateFunction is a viable replacement for the BSpline.
     """
-    def __init__(self, hidden_size = 5):
+    def __init__(self, hidden_size = 5, basis_function = nn.LeakyReLU()):
         """
         UnivariateFunction initializer. 
 
@@ -18,6 +18,8 @@ class UnivariateFunction(nn.Module):
 
         Args:
             hidden_size - The number of neurons in each hidden layer.
+
+            basis_function - The basis function to be weighted within the output.
         """
         super().__init__()
 
@@ -28,6 +30,17 @@ class UnivariateFunction(nn.Module):
             nn.LeakyReLU(),
             nn.Linear(hidden_size, 1)
         )
+
+        if basis_function:
+            self.layer_weight = nn.Parameter(torch.tensor(1)) # As specified in KAN paper
+            self.bias_weight = nn.Parameter(torch.empty(1))
+            torch.nn.init.xavier_uniform_(self.bias_weight) # As specified in KAN paper
+            self.basis_function = basis_function
+        else:
+            # If basis_function = None, weights should not be trainable - no basis function bias.
+            self.layer_weight = nn.Parameter(torch.tensor(1.0), requires_grad = False)
+            self.bias_weight = nn.Parameter(torch.tensor(0.0), requires_grad = False)
+            self.basis_function = lambda x: x
 
     def forward(self, x):
         """
@@ -41,7 +54,10 @@ class UnivariateFunction(nn.Module):
         Returns:
             x - Real-valued activation function output.
         """
-        x = self.layers(x)
+        bias_component = self.bias_weight * self.basis_function(x)
+        layer_component = self.layer_weight * self.layers(x)
+
+        x = bias_component + layer_component
 
         return x
     
@@ -56,7 +72,7 @@ class KANLayer(nn.Module):
     within this layer will be equal to the number of input neurons times
     the number of output neurons.
     """
-    def __init__(self, input_size, output_size, hidden_size = 5):
+    def __init__(self, input_size, output_size, hidden_size = 5, basis_function = nn.LeakyReLU()):
         """
         KANLayer Initializer. Requires input_size and output_size to ensure
         KANLayer is aware of quantity of activation functions required for
@@ -73,17 +89,19 @@ class KANLayer(nn.Module):
 
             hidden_size - The number of neurons in each hidden layer of the
             activation functions.
+
+            basis_function - The basis function to be weighted within each
+            activation function's output.
         """
         super().__init__()
 
         # OUTPUT rows by INPUT columns matrix of functions
         self.activation_matrix = nn.ModuleList(
-        [nn.ModuleList([UnivariateFunction(hidden_size) for _ in range(input_size)])]
+        [nn.ModuleList([UnivariateFunction(hidden_size, basis_function) for _ in range(input_size)])]
         for _ in range(output_size))
 
         self.input_size = input_size
         self.output_size = output_size
-        self.hidden_size = hidden_size
 
     def forward(self, x):
         """
@@ -96,15 +114,15 @@ class KANLayer(nn.Module):
         a batch will have a corresponding k output nodes.
 
         Args:
-            x - Input, passed as 2D tensor: Batch x Number of Features 
+            x - Input, passed as 2D tensor: (Batch, Number of Features)
 
         Returns:
-            x - Output, returned as 2D tensor: Batch x Number of Output Nodes (output_size)
+            x - Output, returned as 2D tensor: (Batch, Number of Output Nodes)
         """
-        # Assert that the number of input dimensions equals the number of input functions
-        assert len(x[0]) == self.input_size
+        batch_size, num_features = x.shape
 
-        batch_size = len(x)
+        # Assert that the number of input dimensions per instance equals the number of input functions
+        assert num_features == self.input_size
 
         row_batch_activations = list()
 
@@ -122,3 +140,57 @@ class KANLayer(nn.Module):
         activations = torch.cat(row_batch_activations, dim = 1)
 
         return activations
+    
+class KAN(nn.Module):
+    """
+    Represents a complete, full implementation of a
+    Kolmogorov-Arnold Network, allowing for multiple input and output
+    dimensions, as well as flexible layer width and depth.
+
+    Inspired by research paper introducing the subject: https://arxiv.org/abs/2404.19756.
+    """
+    def __init__(self, input_size, output_size, num_hidden_layer_output_neurons = [5, 3], hidden_size = 5, basis_function = nn.LeakyReLU()):
+        """
+        KAN Initializer. Requires knowledge of the input and output size
+        ahead of time to set up KANLayer interaction.
+
+        Args:
+            input_size - The dimension of each input instance
+
+            output_size - The number of logits to be output by the network
+
+            num_hidden_layer_output_neurons - The number of output neurons
+            in each of the hidden layers of the KAN
+
+            hidden_size - The number of neurons in each hidden layer of the
+            activation functions
+
+            basis_function - The basis function to be weighted within each
+            activation function's output.
+        """
+        super().__init__()
+
+        self.layers = nn.ModuleList()
+        prev_dim = input_size
+
+        for num_outs in num_hidden_layer_output_neurons:
+            self.layers.append(KANLayer(prev_dim, num_outs, hidden_size, basis_function))
+            prev_dim = num_outs
+
+        self.layers.append(KANLayer(prev_dim, output_size, hidden_size, basis_function))
+
+    def forward(self, x):
+        """
+        Sequentially applies each layer's forward operation starting from
+        the input batch.
+
+        Args:
+            x - Input, passed as 2D tensor: (Batch, Number of Features)
+
+        Returns:
+            x - Output, returned as 2D tensor: (Batch, Number of Output Nodes)
+        """
+        for layer in self.layers:
+            x = layer(x)
+
+        return x

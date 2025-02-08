feat: pad masking implementation during multi-head attention; fix: moved some encoder/decoder variables from local to instance

transformer/attention.py
@@ -31,7 +31,7 @@ class MultiHeadAttention(nn.Module):
 
         self.W_O = nn.Linear(d_model, d_model)
         
-    def forward(self, in_Q: torch.Tensor, in_K: torch.Tensor, in_V: torch.Tensor):
+    def forward(self, in_Q: torch.Tensor, in_K: torch.Tensor, in_V: torch.Tensor, unpadded_seq_lengths: list[int]):
         """
         Pushes the input embedding E through a multi-head attention mechanism,
         returning a tensor of the same dimensions. The embeddings in the output
@@ -45,6 +45,7 @@ class MultiHeadAttention(nn.Module):
             shape (batch_size, seq, d_model). 
             in_V - The V input embedding to be passed through the attention mechanism of
             shape (batch_size, seq, d_model). 
+            unpadded_seq_lengths - A batch_size-length list of the original, unpadded sequence lengths, for attention padding masking
 
         Returns:
             MHA - The result of a Multi-head Attention mechanism on E of shape
@@ -86,12 +87,23 @@ class MultiHeadAttention(nn.Module):
 
         Q_KT_scaled = Q_KT / math.sqrt(self.d_k)
 
+        # CAUSAL MASK
         # If enabled, ensure that seq_i in Q cannot see seq_j in K when j > i.
         # In other words, do not let earlier words (in Q) attend to later words (in K).
         if self.causal_mask:
-            neg_inf_matrix = torch.full_like(Q_KT_scaled, float('-inf'))
-            neg_inf_mask = neg_inf_matrix.triu(diagonal = 1)
-            Q_KT_scaled += neg_inf_mask
+            neg_inf_matrix = torch.full_like(Q_KT_scaled[0][0], float('-inf'))
+            neg_inf_mask = neg_inf_matrix.triu(diagonal = 1) # if called on 4D tensor, triu applies batch*h times to each (seq, seq) dimension, only operates on 2D matrix
+            Q_KT_scaled += neg_inf_mask # if 2D tensor, (seq, seq) will broadcast into (batch_size, h, seq, seq) to mask each (seq, seq)
+
+        # TODO: Parallelize this over the batch dimension too!
+        # SEQ PAD MASK
+        for batch_idx in range(Q_KT_scaled.size(dim = 0)):
+            # Mask out rows outside of this batch element's sequence length
+            Q_KT_scaled[batch_idx][:, unpadded_seq_lengths[batch_idx]:] = float('-inf')
+            # Mask out columns outside of this batch element's sequence length
+            Q_KT_scaled[batch_idx][:, :, unpadded_seq_lengths[batch_idx]:] = float('-inf')
+
+        assert Q_KT_scaled.size() == (in_batch_size, self.num_attention_heads, in_seq, in_seq)
 
         # softmax should be applied row-wise on the (seq, seq) internal matrix. Thus, batch_size * h * seq softmaxes will occur.
         # dim = -1 used to pull out and softmax each row (by collapsing in the column/last dimension).

transformer/decoder.py
@@ -10,7 +10,7 @@ class Decoder(nn.Module):
     encoder's output K and V, and feeds this forward through an MLP to obtain
     the final contextual embeddings.
     """
-    def __init__(self, d_model: int, num_attention_heads: int, d_ff: int, encoder_K: torch.Tensor, encoder_V: torch.Tensor, activation: nn.Module = nn.ReLU(), layer_norm_epsilon: float = 1e-5):
+    def __init__(self, d_model: int, num_attention_heads: int, d_ff: int, activation: nn.Module = nn.ReLU(), layer_norm_epsilon: float = 1e-5):
         """
         Decoder initializer.
 
@@ -18,8 +18,6 @@ class Decoder(nn.Module):
             d_model - The embedding & hidden dimension of the transformer
             num_attention_heads -- The number of attention heads
             d_ff - The feed-forward layer's hidden dimension 
-            encoder_K - The K tensor output by the final encoder block.
-            encoder_V - The V tensor output by the final encoder block.
             activation - The activation function to use in the hidden linear layer at the end of each encoder/decoder block
             layer_norm_epsilon - The epsilon (numerical stability) to use for each LayerNorm layer
         """
@@ -31,9 +29,6 @@ class Decoder(nn.Module):
         self.activation = activation
         self.layer_norm_epsilon = layer_norm_epsilon
 
-        self.encoder_K = encoder_K
-        self.encoder_V = encoder_V
-
         self.masked_mha = MultiHeadAttention(self.d_model, self.num_attention_heads, causal_mask = True)
         self.mha = MultiHeadAttention(self.d_model, self.num_attention_heads)
         self.ff = FeedForward(self.d_model, self.d_ff, self.activation)
@@ -44,27 +39,31 @@ class Decoder(nn.Module):
         self.layer_norm_2 = nn.LayerNorm(self.d_model, eps = self.layer_norm_epsilon)
         self.layer_norm_3 = nn.LayerNorm(self.d_model, eps = self.layer_norm_epsilon)
 
-    def forward(self, x):
+    def forward(self, x, encoder_K: torch.Tensor, encoder_V: torch.Tensor, target_unpadded_seq_lengths: list[int]):
         """
         Pushes the output embedding through one full transformer deocder sequence.
 
         Output is compatible to be fed to either another decoder or towards the output probability layer.
 
         Args:
-            x - The decoder input
+            x - The decoder input of shape (batch_size, seq, d_model)
+            encoder_K - The K tensor output by the final encoder block.
+            encoder_V - The V tensor output by the final encoder block.
+            target_unpadded_seq_lengths - A batch_size-length list of the original, unpadded sequence lengths, for attention padding masking
 
         Returns:
-            x - The full-context decoder embedding (via (causal) multi-head self-attention and cross-attention mechanisms)
+            x - The full-context decoder embedding (via (causal) multi-head self-attention and cross-attention mechanisms) of
+            shape (batch_size, seq, d_model). Input x.size() = Output x.size()
         """
         original_size = x.size()
 
-        MASKED_MHA = self.masked_mha(x.clone(), x.clone(), x.clone())
+        MASKED_MHA = self.masked_mha(x.clone(), x.clone(), x.clone(), target_unpadded_seq_lengths)
         assert MASKED_MHA.size() == original_size
         x += MASKED_MHA
         x = self.layer_norm_1(x)
         assert x.size() == original_size
 
-        MHA = self.mha(x.clone(), self.encoder_K, self.encoder_V)
+        MHA = self.mha(x.clone(), encoder_K, encoder_V, target_unpadded_seq_lengths)
         assert MHA.size() == original_size
         x += MHA
         x = self.layer_norm_2(x)
@@ -88,8 +87,8 @@ if __name__ == '__main__':
 
     E = torch.randn(batch_size, seq, d_model) 
 
-    decoder = Decoder(d_model, n_heads, d_ff, E.clone(), E.clone())
+    decoder = Decoder(d_model, n_heads, d_ff)
 
-    out: torch.Tensor = decoder(E)
+    out: torch.Tensor = decoder(E, E.clone(), E.clone(), [seq] * batch_size)
 
     assert out.size() == E.size()

transformer/encoder.py
@@ -22,11 +22,11 @@ class Encoder(nn.Module):
             layer_norm_epsilon - The epsilon (numerical stability) to use for each LayerNorm layer
         """
         super().__init__()
-        self.d_model = d_model
-        self.num_attention_heads = num_attention_heads
-        self.d_ff = d_ff
-        self.activation = activation
-        self.layer_norm_epsilon = layer_norm_epsilon
+        self.d_model: int = d_model
+        self.num_attention_heads: int = num_attention_heads
+        self.d_ff: int = d_ff
+        self.activation: nn.Module = activation
+        self.layer_norm_epsilon: float = layer_norm_epsilon
 
         self.mha = MultiHeadAttention(self.d_model, self.num_attention_heads)
         self.ff = FeedForward(self.d_model, self.d_ff, self.activation)
@@ -36,7 +36,7 @@ class Encoder(nn.Module):
         self.layer_norm_1 = nn.LayerNorm(self.d_model, eps = self.layer_norm_epsilon)
         self.layer_norm_2 = nn.LayerNorm(self.d_model, eps = self.layer_norm_epsilon)
 
-    def forward(self, x):
+    def forward(self, x, source_unpadded_seq_lengths: list[int]):
         """
         Pushes the input embedding through one full transformer encoder sequence.
 
@@ -44,13 +44,14 @@ class Encoder(nn.Module):
 
         Args:
             x - The input embedding
+            source_unpadded_seq_lengths - A batch_size-length list of the original, unpadded sequence lengths, for attention padding masking
 
         Returns:
             x - The full-context input embedding (via multi-head self-attention mechanism)
         """
         original_size = x.size()
 
-        MHA = self.mha(x.clone(), x.clone(), x.clone()) # Multi-head Attention Mechanism
+        MHA = self.mha(x.clone(), x.clone(), x.clone(), source_unpadded_seq_lengths) # Multi-head Attention Mechanism
         assert MHA.size() == original_size
         x += MHA # Residual Connection
         x = self.layer_norm_1(x)

transformer/transformer.py
@@ -5,7 +5,7 @@ import numpy as np
 
 from encoder import Encoder
 from decoder import Decoder
-from utils import pad_batch_to_longest
+from utils import pad_batch_to_longest_seq_len
 
 class Transformer(nn.Module):
     """
@@ -59,6 +59,9 @@ class Transformer(nn.Module):
 
         self.vocab_linear = nn.Linear(self.d_model, self.vocab_size)
 
+        self.encoders = nn.ModuleList([Encoder(self.d_model, self.num_attention_heads, self.dim_feedforward, self.activation, self.layer_norm_epsilon) for _ in range(self.num_encoder_layers)])
+        self.decoders = nn.ModuleList([Decoder(self.d_model, self.num_attention_heads, self.dim_feedforward, self.activation, self.layer_norm_epsilon) for _ in range(self.num_decoder_layers)])
+
     def forward(self, source: list[torch.Tensor], target: torch.Tensor):
         """
         Performs one full forward pass through the transformer. 
@@ -66,8 +69,8 @@ class Transformer(nn.Module):
         More detail coming soon.
 
         Args:
-            source -- Encoder input batch. A 1D list of 2D tensor of shape (seq, d_model). Each list element is batch example i.
-            target -- Decoder input batch. A 1D list of 2D tensor of shape (seq, d_model). Each list element is batch example i.
+            source -- Encoder input batch. A 1D list of 2D tensor of shape (seq_i, d_model). Each list element is batch example i.
+            target -- Decoder input batch. A 1D list of 2D tensor of shape (seq_i, d_model). Each list element is batch example i.
 
         Returns:
             next_word_probs -- For each seq_i in decoder, produces the next_word_probabilities
@@ -76,28 +79,27 @@ class Transformer(nn.Module):
         # Validate that d_embedding = d_model
         assert source[0].size(1) == self.d_model
 
-        input_embedding, max_input_seq = pad_batch_to_longest(source, pad_value = 0)
-        output_embedding, max_output_seq = pad_batch_to_longest(target, pad_value = 0)
+        source_unpadded_seq_lengths = [tensor.size(dim = 0) for tensor in source] # For padding masking in attention calculation
+        target_unpadded_seq_lengths = [tensor.size(dim = 0) for tensor in target] # For padding masking in attention calculation
+
+        input_embedding, max_input_seq = pad_batch_to_longest_seq_len(source, pad_value = None)
+        output_embedding, max_output_seq = pad_batch_to_longest_seq_len(target, pad_value = None)
 
         # Encoders (Sequential Processing)
-        encoders = nn.ModuleList([Encoder(self.d_model, self.num_attention_heads, self.dim_feedforward, self.activation, self.layer_norm_epsilon) for _ in range(self.num_encoder_layers)])
-
         encoder_input = input_embedding + self.positional_encodings[:max_input_seq].unsqueeze(0)
 
         encoder_output = encoder_input
         for i in range(self.num_encoder_layers):
-            encoder_output = encoders[i](encoder_output)
+            encoder_output = self.encoders[i](encoder_output, source_unpadded_seq_lengths)
 
         encoder_K, encoder_V = encoder_output.clone(), encoder_output.clone()
 
         # Decoders (Sequential Processing)
-        decoders = nn.ModuleList([Decoder(self.d_model, self.num_attention_heads, self.dim_feedforward, encoder_K, encoder_V, self.activation, self.layer_norm_epsilon) for _ in range(self.num_decoder_layers)])
-
         decoder_input = output_embedding + self.positional_encodings[:max_output_seq].unsqueeze(0)
 
         decoder_output = decoder_input
         for i in range(self.num_decoder_layers):
-            decoder_output = decoders[i](decoder_output)
+            decoder_output = self.decoders[i](decoder_output, encoder_K, encoder_V, target_unpadded_seq_lengths)
 
         # Project to Vocabulary Space
         logits = self.vocab_linear(decoder_output)
@@ -149,3 +151,5 @@ if __name__ == '__main__':
     out = transformer(E, T)
 
     assert out.size() == (batch_size, seq, vocab_size)
+
+    print(out)

transformer/utils.py
@@ -3,7 +3,7 @@ import torch.nn.functional as F
 
 import numpy as np
 
-def pad_batch_to_longest(batch: list[torch.Tensor], pad_value = 0) -> tuple[torch.Tensor, int]:
+def pad_batch_to_longest_seq_len(batch: list[torch.Tensor], pad_value = 0) -> tuple[torch.Tensor, int]:
     """
     Given a batch of sequences (each viewed as a seq amount of d_embedding vectors),
     pads each sequence such that its number of seq vectors is equal to the maximum
7173cb9cd774767ecd8d29e0c4aad63694a8291b
plestz <paullestzcode@gmail.com>
2025-05-17 14:33:32 -0400

    [chore] imports for summarization task

--- transformer/pre_training/text_summarization/dataset.py
+++ transformer/pre_training/text_summarization/dataset.py
@@ -2,3 +2,16 @@ import numpy as np
 import pandas as pd
 import torch
 from torch.utils.data import Dataset
+
+class TextSummarizationDataset(Dataset):
+    """
+    """
+    def __init__(self, sequences: torch.Tensor, summaries: torch.Tensor):
+        self.sequences = sequences
+        self.summaries = summaries
+
+    def __len__(self):
+        return self.sequences.shape[0]
+
+    def __getitem__(self, idx):
+        return self.sequences[idx], self.summaries[idx]

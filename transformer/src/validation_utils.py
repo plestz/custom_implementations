422767bc2e71260942efa533bfea28858dea9009
plestz <paullestzcode@gmail.com>
2025-05-19 17:42:57 -0400

    [feat] autoregressive inference can now utilize tokenizer decoder to display predicted text sequences

--- transformer/src/validation_utils.py
+++ transformer/src/validation_utils.py
@@ -4,9 +4,13 @@ import torch
 import torch.nn as nn
 from torch.utils.data import DataLoader
 
+from transformers import PreTrainedTokenizerFast
+
 from src.decoding_strategies import greedy_decode
 
 from tqdm import tqdm
+
+from typing import Union, Callable
 
 def run_gold_validation_loop(dataloader: DataLoader, model: nn.Module, loss_fn: nn.Module, calculate_sequence_accuracy: bool = False, calculate_token_accuracy: bool = False):
     """
@@ -60,7 +64,7 @@ def run_gold_validation_loop(dataloader: DataLoader, model: nn.Module, loss_fn: 
 
     return average_epoch_loss, average_epoch_sequence_accuracy, average_epoch_token_accuracy
 
-def run_autoregressive_validation_loop(dataloader: DataLoader, model: nn.Module, vocab_map: dict, special_token_idxs: dict, max_context_window: int):
+def run_autoregressive_validation_loop(dataloader: DataLoader, model: nn.Module, vocab_map: Union[dict, Callable], special_token_idxs: dict, max_context_window: int):
     """
     Runs one autoregressive validation epoch (processing the entire validation dataset once). 
 
@@ -87,12 +91,18 @@ def run_autoregressive_validation_loop(dataloader: DataLoader, model: nn.Module,
             np_source_indices = source.numpy().copy()
             np_pred_target_indices = pred_indices.numpy().copy()
 
-            token_values = np.array(list(vocab_map.values()))
-            predicted_source_tokens = token_values[np_source_indices]
-            predicted_target_tokens = token_values[np_pred_target_indices]
+            if isinstance(vocab_map, dict):
+                token_values = np.array(list(vocab_map.values()))
+                predicted_source_tokens = token_values[np_source_indices]
+                predicted_target_tokens = token_values[np_pred_target_indices]
+            elif isinstance(vocab_map, Callable):
+                predicted_source_tokens = vocab_map(np_source_indices)
+                predicted_target_tokens = vocab_map(np_pred_target_indices)
+            else:
+                raise TypeError('Vocab map not passed with valid type.')
 
             for s, t in zip(predicted_source_tokens, predicted_target_tokens):
-                source_end_index = np.argmax(s == '<PAD>')  if '<PAD>' in s else len(s)
+                source_end_index = np.argmax(s == '<PAD>') if '<PAD>' in s else len(s)
                 target_end_index = np.argmax(t == '<EOS>')
                 if np.array_equal(np.sort(s[:source_end_index]), t[1:target_end_index]):
                     correct_sequences += 1
@@ -106,3 +116,39 @@ def run_autoregressive_validation_loop(dataloader: DataLoader, model: nn.Module,
             total_sequences += predicted_target_tokens.shape[0]
 
     return correct_sequences / total_sequences
+
+def temp_run_autoregressive_validation_loop(tokenizer: PreTrainedTokenizerFast, dataloader: DataLoader, model: nn.Module, vocab_size: int, vocab_map: dict, special_token_idxs: dict, max_context_window: int):
+    """
+    Runs one autoregressive validation epoch (processing the entire validation dataset once). 
+
+    Args:
+        tokenizer - The pretrained tokenizer to use to decode greedy sequences
+        dataloader - The dataloader to process the dataset in BATCH_SIZE batches
+        model - The Encoder-Decoder that is being trained
+        vocab_size - The size of the vocabulary
+        vocab_map - Maps token vocabulary indices to the corresponding token value
+        special_token_idxs - The indices corresponding to the SOS, EOS, and PAD tokens
+        max_context_window - The maximum context window that the Transformer can process
+    """
+    model.eval()
+
+    with torch.no_grad():
+        
+        for (source, _), label in tqdm(dataloader):
+
+            # FORWARD
+            pred_indices, pred_logits = greedy_decode(source, model, vocab_size, special_token_idxs, max_context_window)
+
+            np_source_indices = source.numpy().copy()
+            np_pred_target_indices = pred_indices.numpy().copy()
+
+            # token_values = np.array(list(vocab_map.values()))
+            # predicted_source_tokens = token_values[np_source_indices]
+            # predicted_target_tokens = token_values[np_pred_target_indices]
+
+            for s, t in zip(np_source_indices, np_pred_target_indices):
+                decoded_source = tokenizer.decode(s)
+                decoded_predicted_target = tokenizer.decode(t)
+                print(f'{'Source:':<20} {decoded_source}\n{'Predicted Target:':<20} {decoded_predicted_target}', end = '\n\n')
+
+            break
